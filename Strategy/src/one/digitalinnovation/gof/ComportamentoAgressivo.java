bcb6aa5f3685c9feb3bad81234243ceef48c8498
kasvrol <dovalekarolina@gmail.com>
2022-11-14 18:35:17 -0300

    feat: class ComportamentoNormal que retornorá os metodos da interface Comportamento do terceiro modo estabelecido pelo projeto

---
+++ Strategy/src/one/digitalinnovation/gof/ComportamentoAgressivo.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.gof;
+
+public class ComportamentoAgressivo implements Comportamento {
+    @Override
+    public void mover() {
+        System.out.println("Movendo-se agressivamente...");
+
+    }
+
+}
