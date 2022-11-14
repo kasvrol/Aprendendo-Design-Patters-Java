ddf21ba7cf007110b83b9e6bd2dc8c0d5eaa830c
kasvrol <dovalekarolina@gmail.com>
2022-11-14 18:33:51 -0300

    feat: class ComportamentoNormal que retornorá os metodos da interface Comportamento do segundo modo estabelecido pelo projeto

---
+++ Strategy/src/one/digitalinnovation/gof/ComportamentoDefensivo.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.gof;
+
+public class ComportamentoDefensivo implements Comportamento {
+
+    @Override
+    public void mover() {
+        System.out.println("Movendo-se defensivamente...");
+
+    }
+
+}
