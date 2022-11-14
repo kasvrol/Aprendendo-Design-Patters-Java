46f2171c198e91e49906631395da8a972bad9267
kasvrol <dovalekarolina@gmail.com>
2022-11-14 18:41:55 -0300

    feat: class Robo que irá realizar os comportamentos das classe já criadas

---
+++ Strategy/src/one/digitalinnovation/gof/Robo.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.gof;
+
+public class Robo {
+    private Comportamento comportamento;
+
+    public void mover() {
+        comportamento.mover();
+    }
+
+    public void setComportamento(Comportamento comportamento) {
+        this.comportamento = comportamento;
+    }
+
+}
