ad335bd1f596b230dbc224d0e25d57751068de15
kasvrol <dovalekarolina@gmail.com>
2022-11-14 18:46:20 -0300

    feat: class main para teste do código

---
+++ Strategy/src/one/digitalinnovation/gof/App.java
@@ -0,0 +1,23 @@
+package one.digitalinnovation.gof;
+
+public class App {
+    public static void main(String[] args) {
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.mover();
+
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+    }
+}
