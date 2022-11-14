177b262ac7331754d2d11583d2ed5722252616db
kasvrol <dovalekarolina@gmail.com>
2022-11-14 14:57:53 -0300

    feat: class ComportamentoNormal que retornorá os metodos da interface Comportamento do primeiro modo estabelecido pelo projeto

---
+++ Strategy/src/one/digitalinnovation/gof/ComportamentoNormal.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.gof;
+
+public class ComportamentoNormal implements Comportamento {
+
+    @Override
+    public void mover() {
+        System.out.println("Movendo-se normalmente...");
+    }
+}
