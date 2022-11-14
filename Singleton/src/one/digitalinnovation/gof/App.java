1166545f5905b75564d7432995f6ddfeb184bf18
kasvrol <dovalekarolina@gmail.com>
2022-11-14 14:39:21 -0300

    fix: correção do nome dos getters das classes

--- Singleton/src/one/digitalinnovation/gof/App.java
+++ Singleton/src/one/digitalinnovation/gof/App.java
@@ -2,14 +2,14 @@ package one.digitalinnovation.gof;
 
 public class App {
     public static void main(String[] args) {
-        Preguicoso demorado = Preguicoso.getInstacia();
+        Preguicoso demorado = Preguicoso.getInstancia();
         System.out.println(demorado);
-        demorado = Preguicoso.getInstacia();
+        demorado = Preguicoso.getInstancia();
         System.out.println(demorado);
 
-        Apressado rapido = Apressado.getInstacia();
+        Apressado rapido = Apressado.getInstancia();
         System.out.println(rapido);
-        rapido = Apressado.getInstacia();
+        rapido = Apressado.getInstancia();
         System.out.println(rapido);
 
         EncapsulamentoSingleton encapsulado = EncapsulamentoSingleton.getInstancia();
